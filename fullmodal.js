df6e678a5ce7fd086be88b1d7b25e933db741a2b
Abhishek Biswal <wirddin@gmail.com>
2015-11-03 12:38:21 +0530

    Fixed close button after load

--- fullmodal.js
+++ fullmodal.js
@@ -6,15 +6,16 @@ $(document).ready(function(){
         e.preventDefault();
         var modal = $(this).attr("data-modal");
         var url = $(this).attr("data-url");
-        $("#"+modal).load(url).fadeIn('slow');
-    })
-
-    $(".fullmodal-close").click(function(e){
-        e.preventDefault();
-        //alert($(this).html());
-        var parent = $(this).closest("div").attr("id");
-        //alert(parent);
-        $("#"+parent).fadeOut('slow');
+        $("#"+modal).load(url, function(){
+                $(".fullmodal-close").click(function(e){
+                    e.preventDefault();
+                    //alert($(this).html());
+                    var parent = $(this).closest("div").attr("id");
+                    //alert(parent);
+                    $("#"+parent).fadeOut('slow');
+                })
+        })
+        $("#"+modal).fadeIn('slow');
     })
 
 })
